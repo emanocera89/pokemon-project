55c353098d842b13ada9be6635c7e8b0a0da0c28
emanocera89 <ema.nocera.89@gmail.com>
2023-08-24 15:54:36 +0200

    added content to Navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,23 @@
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+
 export default function Navbar() {
-    return (
-        <div className="flex flex-col justify-between px-24 py-3 bg-yellow-300 fixed w-full">
-            Navbar
-        </div>
-    )
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position='static' className='bg-yellow-400 fixed shadow-sm z-50'>
+        <Toolbar>
+          <IconButton edge='start' color='inherit' aria-label='menu' sx={{ mr: 2 }}>
+            <MenuIcon className='text-slate-900' />
+          </IconButton>
+          <Typography variant='h6' color='inherit' component='div' className='text-slate-900'>
+            Pokémon App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
